8c74a93500a2f5d33a0308ff1d29f6484e7623a4
Guilherme Senci <guilherme.g.senci@gmail.com>
2021-08-04 12:15:20 -0300

    todo ideia

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+  <Main
+    icon="home"
+    title="Ínicio"
+    subtitle="Seguestão de ferramenta ToDo"
+  >
+    <div className="display-4">Bem vindo</div>
+    <hr />
+    <p className="mb-0">Sistema desenvolvido para exemplo de ToDo com crud</p>
+  </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.css'
+import Reacto from 'react'
+
+export default props =>
+  <footer className="footer">
+    <span>Desenvolvidor por <strong>GuilhermeSenci</strong>.</span>
+  </footer>
